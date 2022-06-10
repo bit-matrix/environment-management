7bb5e9bc8c79acb0cc7c6332471d610bdd94ca97
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-06-10 15:12:17 +0300

    App: process.env.REACT_APP_MODE

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">{process.env.NODE_ENV === "development" ? process.env.REACT_APP_DEV_MODE : process.env.REACT_APP_PRO_MODE}</div>;
-}
+const App = () => {
+  return <div className="App">{process.env.REACT_APP_MODE}</div>;
+  //return <div className="App">{process.env.NODE_ENV === "development" ? process.env.REACT_APP_DEV_MODE : process.env.REACT_APP_PRO_MODE}</div>;
+};
 
 export default App;
